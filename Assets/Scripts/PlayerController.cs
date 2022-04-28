93fe36f449ccd75008826803a9d73c45a6953019
La Syl <scaffarel@uoc.edu>
2022-04-28 05:39:29 +0200

    Dialogue system + Input Manager
    Dialogue system package added
    Player input manager set & tested : player movement is working fine with UI buttons
    Using transform.Translate to move player for more precise movements.
    Library\ArtifactDB file not included because too big

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,21 +5,20 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody2D rb;
+    private Rigidbody2D rb2d;
     private float movX;
     private float movY;
-    public float speed = 1;
+    public float speed = 10;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     private void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
-
         movX = movementVector.x;
         movY = movementVector.y;
     }
@@ -27,7 +26,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        PlayerMove();
+    }
+
+    //Function that controls player movement
+    private void PlayerMove() 
+    {
         Vector2 movement = new Vector2(movX, movY);
-        rb.AddForce(movement * speed);
+        transform.Translate(movement * speed * Time.deltaTime);
     }
 }
