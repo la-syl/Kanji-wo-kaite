92cbe2c4adcd28d0929df195392d4a53476f98ce
La Syl <scaffarel@uoc.edu>
2022-05-17 05:25:34 +0200

    UI Improvements
    - Added Kanji Animations Menu (unfinished)
    - Added Kanji Animations to kanji draw area
    - Added functionality to the kanji draw area
    - Added variables for user stats
    - Changed Dialogue Manager UI prefabs (unfinished)
    - Deleted useless files (unfinished)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,30 +5,35 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    //private Rigidbody2D rb2d;
+    private Rigidbody2D rb2d;
     private float movX;
     private float movY;
     public float speed = 10;
     private Animator animator;
 
+    private Vector2 movement = Vector2.zero;
+    private Vector2 movementVector = Vector2.zero;
+
     // Start is called before the first frame update
     void Awake()
     {
-        //rb2d = GetComponent<Rigidbody2D>();
+        rb2d = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
     }
 
     private void OnMove(InputValue movementValue)
     {
-        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementVector = movementValue.Get<Vector2>();
         movX = movementVector.x;
         movY = movementVector.y;
+
+        Debug.Log("Joystick Movement Detected");
+        Debug.Log("X value: " + movX + " - Y value: " + movY);
 
         if (movX != 0 || movY != 0)
         {
             animator.SetFloat("X", movX);
             animator.SetFloat("Y", movY);
-
             animator.SetBool("IsWalking", true);
         }
         else 
@@ -45,12 +50,14 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         PlayerMove();
+        Debug.Log("movement vector : " + movementVector);
     }
 
     //Function that controls player movement
     private void PlayerMove() 
     {
-        Vector2 movement = new Vector2(movX, movY);
-        transform.Translate(movement * speed * Time.deltaTime);
+        movement.Set(movX, movY);
+        rb2d.velocity = movement * speed * Time.deltaTime;
+        Debug.Log("Current player speed : " + movement);
     }
 }
---
+++ Assets/UI/GestureRecognizer/Scripts/MyGestureHandler.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using GestureRecognizer;
+using System.Linq;
+using TMPro;
+using System;
+using PixelCrushers.DialogueSystem;
+
+public class MyGestureHandler : MonoBehaviour {
+
+	public TMP_Text textResult;
+	public TMP_Text score;
+	public Transform referenceRoot;
+	private GesturePatternDraw[] references;
+
+	//HonQuest variables
+	public GameObject JishoButton;
+	private int timesHonWritten = 0;
+
+	private RecognitionResult cachedResult;
+
+	void Start () {
+		references = referenceRoot.GetComponentsInChildren<GesturePatternDraw> ();
+	}
+
+	public void OnRecognize(RecognitionResult result){
+		//StopAllCoroutines ();
+		if (result != RecognitionResult.Empty) {
+			Debug.Log("Recognized!");
+			cachedResult = result;
+			textResult.text = result.gesture.id;
+			score.text = Mathf.RoundToInt(result.score.score * 100) + "%";
+			int oldKanjiValue = DialogueLua.GetVariable("TotalNumberOfKanjisWritten").asInt;
+			DialogueLua.SetVariable("TotalNumberOfKanjisWritten", oldKanjiValue+1);
+		} else {
+			textResult.text = "Try again";
+		}
+	}
+
+	public void KanjiCheck() {
+		if (cachedResult.gesture.id == "Hon")
+		{
+
+			bool LearnedHon = DialogueLua.GetVariable("wroteHon").asBool;
+
+			if (LearnedHon == false) {
+				DialogueLua.SetVariable("wroteHon", true);
+				JishoButton.SetActive(true);
+			}
+
+			Debug.Log("GG! Tu viens d'écrire le Kanji Hon! ");
+			timesHonWritten++;
+
+			if (timesHonWritten == 10) {
+				DialogueManager.ShowAlert("You have written " + timesHonWritten + " times!");
+			}
+		}
+		if (cachedResult.gesture.id == "Ichi") 
+		{
+			Debug.Log("You have written " + cachedResult.gesture.id);
+		}
+		if (cachedResult.gesture.id == "Ni")
+		{
+			Debug.Log("You have written " + cachedResult.gesture.id);
+		}
+		if (cachedResult.gesture.id == "San")
+		{
+			Debug.Log("You have written " + cachedResult.gesture.id);
+		}
+		if (cachedResult.gesture.id == "Yon")
+		{
+			Debug.Log("You have written " + cachedResult.gesture.id);
+		}
+		if (cachedResult.gesture.id == "Go")
+		{
+			Debug.Log("You have written " + cachedResult.gesture.id);
+		}
+	}
+
+}
